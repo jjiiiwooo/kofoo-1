ec2e96bf9e32080ae93c5abe4736d836058986bf
jjiiiwooo <jiuuchoe@gmail.com>
2023-11-29 17:24:53 +0900

    메인페이지에 현재 위치 띄우기

--- src/components/Camera.jsx
+++ src/components/Camera.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef, useCallback, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import Webcam from "react-webcam";
 //import axios from "axios";
@@ -9,16 +9,40 @@ import { FcOk } from "react-icons/fc";
 
 
 
-const videoConstraints = {
+/*const videoConstraints = {
     facingMode: 'user'
-};
+};*/
 
 const Camera = () => {
     const webcamRef = useRef(null);
+    const [selectedDeviceId, setSelectedDeviceId] = useState([]);
     const [imgSrc, setImgSrc] = useState(null);
     const [submit, setSubmit] = useState(false);
     const navigate = useNavigate();
 
+    useEffect(() => {
+        const getDevices = async () => {
+          try {
+            const devices = await navigator.mediaDevices.enumerateDevices();
+            const videoDevices = devices.filter(device => device.kind === 'videoinput');
+            
+            if (videoDevices.length > 0) {
+              setSelectedDeviceId(videoDevices[0].deviceId); // 첫 번째 웹캠을 선택
+              alert("사용자의 카메라에 접근합니다.");
+            }
+          } catch (error) {
+            alert('Error getting media devices:', error);
+          }
+        };
+    
+        if (navigator.mediaDevices && navigator.mediaDevices.enumerateDevices) {
+          getDevices();
+        } else {
+          alert('enumerateDevices() not supported.');
+        }
+      }, []);
+
+    
     //현재 webcam 이미지의 base64 인코딩 문자열 반환
     const onCapture = useCallback(()=> {
         const imageSrc = webcamRef.current.getScreenshot();
@@ -44,18 +68,21 @@ const Camera = () => {
             {imgSrc ? (
                 <img src={imgSrc} alt="webcam" />
             ):(
-                <Webcam className="webcamstyle"
-                    audio={false}
-                    height={1280}
-                    width={600}
-                    ref={webcamRef}
-                    screenshotFormat="image/jpeg"
-                    videoConstraints={videoConstraints}
-                 />
+                <div>
+                    {selectedDeviceId? (
+                        <Webcam className="webcamstyle"
+                         audio={false}
+                         mirrored={true}
+                         height={680}
+                         width={600}
+                         screenshotFormat="image/jpeg"
+                        videoConstraints={{deviceId: selectedDeviceId}} ref={webcamRef} />
+                    ): (
+                        <p>사용자의 카메라를 가져올 수 없습니다.</p>
+                    )}
+                </div>
+             )}
 
-                 
-
-            )}
             <div>
               {imgSrc ? (
                 <FcRedo className="redostyle" onClick={onRetake}>재촬영</FcRedo>
